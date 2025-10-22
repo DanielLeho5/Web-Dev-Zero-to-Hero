1ea767f0bf10e5bf4f03b4ab071c666d1090d74c
Daniel Lehoczki <lehoczki.dani5@gmail.com>
2025-10-22 19:46:54 +0200

    quantity selctor functionality

--- SupersimpleDev_JS_Tutorial/section_13/scripts/amazon.js
+++ SupersimpleDev_JS_Tutorial/section_13/scripts/amazon.js
@@ -24,7 +24,7 @@ products.forEach((product) => {
         </div>
 
         <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
             <option selected value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -58,6 +58,8 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     button.addEventListener('click', () => {
         const productId = button.dataset.productId;
+        const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+        const quantityValue = Number(quantitySelector.value);
 
         let matchingItem;
         cart.forEach(item => {
@@ -67,11 +69,11 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         })
 
         if (matchingItem) {
-            matchingItem.quantity++;
+            matchingItem.quantity += quantityValue;
         } else {
             cart.push({
                 productId,
-                quantity: 1
+                quantity: quantityValue
             })
         }
 
