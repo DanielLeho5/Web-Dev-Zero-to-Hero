b0b546c1233dd2482620215db759a98b2ee04552
Daniel Lehoczki <lehoczki.dani5@gmail.com>
2025-10-25 11:01:18 +0200

    make payment summary interactive

--- SupersimpleDev_JS_Tutorial/section_14/scripts/checkout/order-summary.js
+++ SupersimpleDev_JS_Tutorial/section_14/scripts/checkout/order-summary.js
@@ -1,15 +1,9 @@
 import { cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption } from '../../data/cart.js';
 import { products, getProduct } from '../../data/products.js';
 import formatCurrency from '../utils/money.js';
-import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from '../../data/delivery-option.js';
-
-//hello();
-
-//const today = dayjs();
-//const deliveryDate = today.add(7, 'days')
-//console.log(deliveryDate.format('dddd, MMMM D'))
+import { renderPaymentSummary } from './payments-summary.js';
 
 export function renderOrderSummary() {
 
@@ -117,6 +111,7 @@ export function renderOrderSummary() {
             const container = document.querySelector(`.js-cart-item-container-${productId}`);
             container.remove();
             updateCheckoutCartQuantity();
+            renderPaymentSummary();
         })
     })
 
@@ -176,6 +171,7 @@ export function renderOrderSummary() {
             const { productId, deliveryOption } = element.dataset;
             updateDeliveryOption(productId, deliveryOption);
             renderOrderSummary();
+            renderPaymentSummary();
         })
     })
     
