379871c41190e456defe67d45dd650a2cd93cf7e
Daniel Lehoczki <lehoczki.dani5@gmail.com>
2025-10-26 11:47:35 +0100

    create automated tests for addToCart()

--- SupersimpleDev_JS_Tutorial/section_16/data/cart.js
+++ SupersimpleDev_JS_Tutorial/section_16/data/cart.js
@@ -1,25 +1,30 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
+loadFromStorage();
 
-if (!cart || cart.length === 0) {
-    cart = [
-        {
-            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            quantity: 2,
-            deliveryOptionId: '1'
-        },
-        {
-            productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-            quantity: 1,
-            deliveryOptionId: '2'
-        }
-    ];
+export function loadFromStorage() {
+    cart = JSON.parse(localStorage.getItem('cart'));
+
+    if (!cart) {
+        cart = [
+            {
+                productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                quantity: 2,
+                deliveryOptionId: '1'
+            },
+            {
+                productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+                quantity: 1,
+                deliveryOptionId: '2'
+            }
+        ];
+    }
 }
 
 function saveToStorage() {
     localStorage.setItem('cart', JSON.stringify(cart));
 }
 
-export function addToCart(productId, quantity) {
+export function addToCart(productId, quantity = 1) {
     let matchingItem;
     cart.forEach(cartItem => {
         if (productId === cartItem.productId) {
@@ -32,7 +37,7 @@ export function addToCart(productId, quantity) {
     } else {
         cart.push({
             productId,
-            quantity,
+            quantity: 1,
             deliveryOptionId: '1'
         })
     }
